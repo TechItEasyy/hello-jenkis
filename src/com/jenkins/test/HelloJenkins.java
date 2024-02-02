c5aa0b360d57007560ccad38885b0b045bc7756a
bigbangdatapoint <techiteasyy@gmail.com>
2024-02-02 22:39:35 +0530

    initial project

---
+++ src/com/jenkins/test/HelloJenkins.java
@@ -0,0 +1,10 @@
+package com.jenkins.test;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins!!!");
+		System.out.println("Hell Java!!!");
+	}
+
+}
